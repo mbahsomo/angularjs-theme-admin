56db66916ad9c89baf07fc6a90d7d424ecfd2154
Sugik Puja Kusuma <mbahsomo@do-event.com>
2014-07-02 05:57:48 +0700

    modified:   assets/app/awal.html 	modified:   assets/app/awal.js

--- assets/app/awal.js
+++ assets/app/awal.js
@@ -1,8 +1,43 @@
 angular.module('ThemeApp').controllerProvider.register('AwalController', function($scope, $http, $document, toaster){
 	$scope.dataRec = [];
 	$scope.dataRecSelect = [];
+    
+    $scope.LoadGrid = function () {
+        for (a = 0 ; a <100; a++){
+            $scope.dataRec[a] = {name:'Data ke ' + a, age : a}; 
+        }
+    };
 
-	$scope.KlikEvnt = function(row){
+    $scope.LoadGrid();
+
+    $scope.gridOptions = {
+        data: 'dataRec' ,
+        columnDefs: [
+            {field:'name', displayName:'Name', resizable : true}, 
+            {field:'age', displayName:'Age', width: '100px', resizable : true},
+            {
+                displayName:'Event',
+                cellTemplate : '<button class="btn btn-default btn-xs" ng-click="KlikEvnt(row.entity)"><i class="glyphicon glyphicon-pencil"></i> Edit</button>',
+                width: '100px'
+            }
+        ],
+        enablePaging: true,
+        //headerRowHeight : 60,
+        showFooter: true,
+        multiSelect: false,
+        enablePinning: true,
+        showGroupPanel: true,
+        //jqueryUITheme: true,
+        showColumnMenu: true,
+        showSelectionCheckbox: true,
+        totalServerItems: 'totalServerItems',
+        pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions,
+        selectedItems: $scope.dataRecSelect
+        /*rowTemplate :'<div ng-dblclick="onGridDoubleClick(row)" ng-style="{\'cursor\': row.cursor}" ng-repeat="col in visibleColumns()" class="ngCell col{{$index}} {{col.cellClass}}" ng-cell></div>',        */
+    };
+
+    $scope.KlikEvnt = function(row){
 		console.log(row);
 		row.age=212;
 	};
@@ -15,40 +50,6 @@ angular.module('ThemeApp').controllerProvider.register('AwalController', functio
     $scope.onGridDoubleClick = function(row){
         console.log(row);
     };
-
-    $scope.gridOptions = {
-    	data: 'dataRec' ,
-    	columnDefs: [
-    		{field:'name', displayName:'Name', resizable : true}, 
-    		{field:'age', displayName:'Age', width: '100px', resizable : true},
-    		{
-    			displayName:'Event',
-    			cellTemplate : '<button class="btn btn-default btn-xs" ng-click="KlikEvnt(row.entity)"><i class="glyphicon glyphicon-pencil"></i> Edit</button>',
-    			width: '100px'
-    		}
-    	],
-    	enablePaging: true,
-		showFooter: true,
-		multiSelect: false,
-      	enablePinning: true,
-      	showGroupPanel: true,
-      	//jqueryUITheme: true,
-      	showColumnMenu: true,
-        showSelectionCheckbox: true,
-        totalServerItems: 'totalServerItems',
-        pagingOptions: $scope.pagingOptions,
-        filterOptions: $scope.filterOptions,
-        selectedItems: $scope.dataRecSelect
-        /*rowTemplate :'<div ng-dblclick="onGridDoubleClick(row)" ng-style="{\'cursor\': row.cursor}" ng-repeat="col in visibleColumns()" class="ngCell col{{$index}} {{col.cellClass}}" ng-cell></div>',        */
-    };
-
-    $scope.LoadGrid = function () {
-    	for (a = 0 ; a <100; a++){
-    		$scope.dataRec[a] = {name:'Data ke ' + a, age : a};	
-    	}
-    };
-
-    $scope.LoadGrid();
 
     $scope.ShowAdd = function (){
     	angular.element('#window-modal').modal(
@@ -76,6 +77,16 @@ angular.module('ThemeApp').controllerProvider.register('AwalController', functio
                 $scope.dataRec.splice($scope.dataRec.indexOf(rowItem),1);
             });
         }
+    };
+
+    $scope.ShowFilter = function(){
+        angular.element('#window-filter').modal(
+            {
+                top : '150px',
+                backdrop:false,
+                keyboard:false
+            }
+        );
     };
 
     $scope.Reload = function(){
